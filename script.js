06b96c427732e2bfd335553f26b5d6a4e344f398
Logan <92747680+celestial-bean@users.noreply.github.com>
2024-04-27 12:58:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function computerPlay() {
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');function computerPlay() {
   let pick = Math.floor(Math.random() * 3);
 
   if (pick === 2) {
